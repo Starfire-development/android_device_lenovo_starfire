2fb1ad9f8ba03023376d476453e6ee8935030b7c
Ethan Chen <intervigil@gmail.com>
2019-10-18 23:19:20 -0700

    starfire: Remove firmware dirs from config.fs
    Change-Id: I68a542994790cea244d18b6244224a4faee6ba56

--- config.fs
+++ config.fs
@@ -43,12 +43,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
 [persist/]
 mode: 0771
 user: AID_SYSTEM
