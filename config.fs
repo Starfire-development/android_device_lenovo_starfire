1996bb651eca8ae6b4992cf9e2177806e1972fb9
Ethan Chen <intervigil@gmail.com>
2019-10-20 18:17:41 -0700

    starfire: Remove bogus config.fs entries
    * Entries for /system/vendor/bin and /system/bin are not valid in our
      configuration, since mata has a real vendor partition. Remove these
      unused entries.
    
    Change-Id: I339bbd03e97d62aba20677a624c1a18a6f729904

--- config.fs
+++ config.fs
@@ -42,15 +42,3 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
-caps: 0
