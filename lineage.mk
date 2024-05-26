b511948c153fe672a799003898a728fcd6bdee79
electimon <electimon@gmail.com>
2024-05-26 01:36:08 +0000

    starfire: Remove telephony, correct launch api
    Change-Id: I244ba8b9df40090b757072a4051000222c49c49a
    Signed-off-by: electimon <electimon@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -21,8 +21,8 @@ TARGET_SCREEN_WIDTH := 1920
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, device/lenovo/starfire/verity.mk)
 
 # Inherit some common LineageOS stuff.
