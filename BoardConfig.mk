c2745bc42ab4685827d4fd16bfb3abf032252cae
electimon <electimon@gmail.com>
2024-05-27 00:43:56 +0000

    starfire: Switch to armv8-a
    Signed-off-by: electimon <electimon@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_BOARD_PLATFORM := msm8953
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
